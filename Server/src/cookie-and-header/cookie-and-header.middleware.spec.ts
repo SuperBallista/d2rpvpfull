8f43f55b1b674875aa37669437a21606ebf36f3a
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2024-12-12 16:57:22 +0900

    테스트

--- Server/src/cookie-and-header/cookie-and-header.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CookieAndHeaderMiddleware } from './cookie-and-header.middleware';
-
-describe('CookieAndHeaderMiddleware', () => {
-  it('should be defined', () => {
-    expect(new CookieAndHeaderMiddleware()).toBeDefined();
-  });
-});
--- Server/src/jwt-auth/jwt-auth.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { JwtAuthMiddleware } from './jwt-auth.middleware';
-
-describe('JwtAuthMiddleware', () => {
-  it('should be defined', () => {
-    expect(new JwtAuthMiddleware()).toBeDefined();
-  });
-});
