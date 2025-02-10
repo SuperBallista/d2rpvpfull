aba12f276e275a49a4e177fa49ff485ecd56854d
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2025-02-11 03:55:11 +0900

    운영진 권한 부여

--- Server/src/config/constants.ts
+++ Server/src/config/constants.ts
@@ -41,6 +41,6 @@ export const TEAMS_SCORES_Z = {
 export const EVENT_HOST_SCORE = 3;
 
 
-export const BABAPK_ADMIN = ["kor8240@gmail.com", "mhj2030@hanmail.net"]
+export const BABAPK_ADMIN = ["kor8240@gmail.com", "mhj2030@hanmail.net", "acertn@gmail.com"]
 export const MPK_ADMIN = ["kor8240@gmail.com", "mhj2030@hanmail.net", "3864769874", "3864854217"]
 export const ZPKE_ADMIN = ["kor8240@gmail.com", "mhj2030@hanmail.net", "ari", "ericpark909@gmail.com", "3865796655"]
