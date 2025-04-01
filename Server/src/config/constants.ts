6044354a92cc94d20d3387dec76df1d90cb12870
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2025-04-01 18:16:39 +0900

    zpke 관리자를 추가합니다

--- Server/src/config/constants.ts
+++ Server/src/config/constants.ts
@@ -18,7 +18,7 @@ export const TEAMS_SCORES_B = {
   teams4: { Championship: 21, Runner_up: 11, Place3rd:0 },
   teams8: { Championship: 42, Runner_up: 22, Place3rd: 12 },
   teams16: { Championship: 83, Runner_up: 43, Place3rd: 23 },
-  teams24: { Championship: 205, Runner_up: 105, Place3rd: 55 },
+  teams24: { Championship: 150, Runner_up: 75, Place3rd: 40 },
 };
 
 export const TEAMS_SCORES_M = {
@@ -43,4 +43,4 @@ export const EVENT_HOST_SCORE = 3;
 
 export const BABAPK_ADMIN = ["kor8240@gmail.com", "mhj2030@hanmail.net", "acertn@gmail.com", "carnby75@gmail.com", "yym08055@gmail.com"]
 export const MPK_ADMIN = ["kor8240@gmail.com", "mhj2030@hanmail.net", "3864769874", "3867505568"]
-export const ZPKE_ADMIN = ["kor8240@gmail.com", "mhj2030@hanmail.net", "ericpark909@gmail.com", "3865796655", "3865865713"]
+export const ZPKE_ADMIN = ["kor8240@gmail.com", "mhj2030@hanmail.net", "ericpark909@gmail.com", "3865796655", "3865865713", "3865199209"]
